555639d48d256c44c0243788cfd2bb3c3db5c824
Ny Nguyen <nnguyen110326@gmail.com>
2026-04-07 17:06:49 -0500

    lab 9 draft

---
+++ script.js
@@ -0,0 +1,15 @@
+function init(){
+//add your javascrip between these two lines of code
+  var button = document.getElementById('entrybutton');
+  var textbox = document.getElementById('entryinput');
+  var output = document.getElementById('textoutput');
+
+  function eventClick(){
+    var value = textbox.value;
+    alert("Ny Nguyen: " + value);
+    output.textContent = value;
+  }
+  
+  button.addEventListener('click', eventClick);
+}
+window.addEventListener('load', init);
